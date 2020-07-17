90f891fd5470dc4caeefabce5f1aff9effd42d9d
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-17 16:04:20 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -23,7 +23,7 @@ def consolidate_cart(cart)
   cart.each do |item_info|
     item_name = item_info[:item]
       check = find_item_by_name_in_collection(item_name, cons_cart)
-      if check.class == array
+      if check.class == NilClass 
         cons_cart.push(item)
         binding.pry
       else
