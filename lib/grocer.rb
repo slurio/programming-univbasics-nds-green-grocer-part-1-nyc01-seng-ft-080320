977389dce6ee2ebad22610dd1feadaf032904cd7
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-17 16:39:58 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -29,7 +29,7 @@ def consolidate_cart(cart)
       else
         cons_cart.each do |organzied_item_info|
           if organzied_item_info == item_info
-            cons_cart[:count] += 1
+            cons_cart[:count] = 1
             binding.pry
           end
         end
